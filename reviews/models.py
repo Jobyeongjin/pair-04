bf50ab8b2eb5d1181b3a0d7cebfbc356189f92df
wook <ifol1129@gmail.com>
2022-10-21 17:21:46 +0900

    add Likes

--- reviews/models.py
+++ reviews/models.py
@@ -11,6 +11,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
 class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
