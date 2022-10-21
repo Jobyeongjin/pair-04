93a7e56e53eeccbeae1c1cd6193ba7b663722eab
wook <ifol1129@gmail.com>
2022-10-21 10:54:02 +0900

    signup/update

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path("login/", views.login, name="login"),
+    path("profile/<int:user_pk>", views.profile, name="profile"),
+    path("update/<int:user_pk>", views.update, name="update"),
+    path("password/<int:user_pk>", views.change_password, name="change_password"),
 ]
