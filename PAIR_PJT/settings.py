1fe4ad97806a202797058d9875d3088ced16f6aa
wook <ifol1129@gmail.com>
2022-10-21 10:24:11 +0900

    accounts/signup

--- PAIR_PJT/settings.py
+++ PAIR_PJT/settings.py
@@ -40,8 +40,10 @@ ALLOWED_HOSTS = []
 MEDIA_ROOT = BASE_DIR / 'images'
 MEDIA_URL = '/media/'
 
+AUTH_USER_MODEL = 'accounts.User'
+
 INSTALLED_APPS = [
-    
+    'accounts',
     'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
